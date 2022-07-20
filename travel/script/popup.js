b9529cb860d22e26e8c4733e81730ef93bb3991b
Andrey Efremov <samo.dur.andre@gmail.com>
2022-07-20 04:10:09 +0300

    feat: add popup markup, style and js

--- travel/script/popup.js
+++ travel/script/popup.js
@@ -1,14 +1,17 @@
 'use strict';
 
 const BTN_LOGIN = document.querySelector('.button-login__link');
-const POPUP = document.querySelector('.popup-background');
+const POPUP_WRAP = document.querySelector('.header__popup-wrapper');
+const POPUP = document.querySelector('.popup__content');
 
 BTN_LOGIN.addEventListener('click', function () {
-	POPUP.classList.toggle('active');
+	POPUP_WRAP.classList.toggle('active');
+	POPUP_WRAP.classList.toggle('bg');
 	console.log('Бац по кнопке логин!');
 })
 
-POPUP.addEventListener('click', function () {
-	POPUP.classList.toggle('active');
+POPUP_WRAP.addEventListener('click', function () {
+	POPUP_WRAP.classList.toggle('active');
+	POPUP_WRAP.classList.toggle('bg');
 	console.log('Бац по серому полю');
 })
