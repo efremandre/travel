27cf1cc655fe4bcf7e1a59332bcfedcbbb4aead9
Andrey Efremov <samo.dur.andre@gmail.com>
2022-07-22 14:47:44 +0300

    feat: add popup

--- travel/script/popup.js
+++ travel/script/popup.js
@@ -1,17 +1,43 @@
 'use strict';
 
 const BTN_LOGIN = document.querySelector('.button-login__link');
-const POPUP_WRAP = document.querySelector('.header__popup-wrapper');
+const MOBILEBTN_LOGIN = document.querySelector('.login__link');
+const BTN_REG = document.querySelector('.popup__footer-link');
+const BTN_SIGIN = document.querySelector('.popupreg-link');
+const POPUP_WRAP = document.querySelector('.popup-wrapper');
 const POPUP = document.querySelector('.popup__content');
+const POPUP_REG = document.querySelector('.popupreg__content');
+const BODY = document.querySelector('body');
 
 BTN_LOGIN.addEventListener('click', function () {
 	POPUP_WRAP.classList.toggle('active');
-	POPUP_WRAP.classList.toggle('bg');
-	console.log('Бац по кнопке логин!');
+	POPUP.classList.toggle('active');
+	BODY.classList.toggle('scroll-non');
 })
 
-POPUP_WRAP.addEventListener('click', function () {
+MOBILEBTN_LOGIN.addEventListener('click', function () {
 	POPUP_WRAP.classList.toggle('active');
-	POPUP_WRAP.classList.toggle('bg');
-	console.log('Бац по серому полю');
+	POPUP.classList.toggle('active');
+	BODY.classList.toggle('scroll-non');
+})
+
+POPUP_WRAP.addEventListener('click', function (e) {
+	console.log('БАЦ по серому')
+	if (!e.target.closest('.popup__content') && !e.target.closest('.popupreg__content')) {
+		POPUP_WRAP.classList.toggle('active');
+		POPUP.classList.toggle('active');
+		BODY.classList.toggle('scroll-non');
+	}
+})
+
+BTN_REG.addEventListener('click', function () {
+	POPUP.classList.toggle('hidden');
+	POPUP_REG.classList.toggle('active');
+})
+
+
+BTN_SIGIN.addEventListener('click', function () {
+	POPUP.classList.toggle('hidden');
+	POPUP_REG.classList.toggle('active');
+	console.log('БАЦ')
 })
